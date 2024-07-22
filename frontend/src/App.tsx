b603f2ebc63b8f124d265c74969ff599b515d627
Todd <sutho100@gmail.com>
2024-07-22 20:12:21 +1000

    feat: Frontend - render table data with key from mockdata

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import MockData from "./data.json";
 
-function App() {
+const App = () => {
+  const { ReportName, ReportTitles, Rows } = MockData.Reports[0];
+
+  const dataRow = (row: any, index: number) => {
+    if (row.RowType === "Section") {
+      return (
+        <>
+          <tr>
+            <th colSpan={3}>{row.Title}</th>
+          </tr>
+          {row.Rows.map(dataRow)}
+        </>
+      );
+    }
+    return (
+      <tr key={index}>
+        {row.Cells.map((cell: any, cellIndex: number) => (
+          <td key={cellIndex}>{cell.Value}</td>
+        ))}
+      </tr>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{ReportName}</h1>
+      <table>
+        <thead>
+          <tr>
+            {ReportTitles.map((title: string, index: number) => (
+              <th key={index} colSpan={4}>
+                {title}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {Rows.map((row: any, index: number) => dataRow(row, index))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
