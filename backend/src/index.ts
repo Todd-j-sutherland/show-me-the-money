f4cc50b2ff1cb9257accbd0e476cd91525a76c0b
Todd <sutho100@gmail.com>
2024-07-23 10:51:28 +1000

    feat: Backend - Add cors package and update dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import axios from "axios";
+import cors from "cors";
 
 const app = express();
 const PORT = 4001;
 const localHost = "http://localhost:3000";
+
+app.use(cors());
 
 app.get("/api/v1/balance-sheet", async (req, res) => {
   try {
